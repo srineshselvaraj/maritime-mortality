54d5ac2cbd49cf9abf352e31605ef1a7c6fa3057
srineshselvaraj <srinesh9@gmail.com>
2023-11-04 15:34:37 -0400

    Update maritime-main.py

--- maritime-main.py
+++ maritime-main.py
@@ -14,5 +14,16 @@ for c in md.columns:
     else:
         md = md.rename(columns={c : c.replace("_age_", " ")[:-1].upper()})
 
-# testing streamlit
-st.dataframe(md)
+st.markdown("<h2 style='text-align: center;'>Maritime Mortality</h2>", unsafe_allow_html=True)
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.text_input('Year')
+with col2:
+    st.text_input('Age')
+with col3:
+    st.text_input('Gender')
+with col4:
+    c1, c2, c3 = st.columns((1, 3, 1))
+    with c2:
+        st.markdown("<style> button {align-items: center} </style>", unsafe_allow_html=True)
+        st.button('Predict')
